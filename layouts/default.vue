646aa6e459a097319320eb117b7be67ced2c7747
c-road <105139975+yas-ako@users.noreply.github.com>
2022-10-13 22:56:56 +0900

    [add] Add header supporting responsive design

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav" />
+    <TheSidenav :show="displaySidenav" @close="displaySidenav = false" />
+
     <Nuxt />
   </div>
 </template>
+<script>
+import TheHeader from '@/components/Navigation/TheHeader'
+import TheSidenav from '@/components/Navigation/TheSidenav'
+
+export default {
+  components: {
+    // Header,
+    TheHeader,
+    TheSidenav,
+  },
+  data() {
+    return {
+      displaySidenav: false,
+    }
+  },
+}
+</script>
