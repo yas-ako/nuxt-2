5795114d3c181d9898015d05b943f6e0d5a2c2d7
c-road <105139975+yas-ako@users.noreply.github.com>
2022-10-13 20:40:33 +0900

    [update] Use Google Fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,11 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;400;500;700&display=swap',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
